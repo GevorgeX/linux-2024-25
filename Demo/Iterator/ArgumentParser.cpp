bef963377a2e84d02943846e1e80f656bfd19f53
GevorgX <gevnavoyan@gmail.com>
2024-10-20 17:23:31 +0400

    Rebase Directory

---
+++ Demo/Iterator/ArgumentParser.cpp
@@ -0,0 +1,16 @@
+#include "Iterator/ArgumentParser.h"
+
+#include <iostream>
+#include <ostream>
+
+int main(int argc, char** argv)
+{
+    ArgumentParser s(argc, argv);
+
+    s.parse("ab:cd");
+
+    for (auto& t : s)
+    {
+        std::cout << t.flag<<" "<<*t.value << std::endl;
+    }
+}
