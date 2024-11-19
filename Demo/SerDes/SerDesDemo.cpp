a95634be13cf1b602ad9d7eb4e23133dec7cd134
Gevorg <gevnavoyan@gmail.com>
2024-11-19 16:19:27 +0400

    Add functions

--- Demo/SerDes/SerDesDemo.cpp
+++ Demo/SerDes/SerDesDemo.cpp
@@ -1,5 +1,24 @@
+#include <iostream>
 #include <SerDes/SerDes.h>
 
-int main() {
+struct  ST {
+    int a;
+    float b;
+    char c;
 
+    ST(){}
+    ST(int a, float b, char c): a(a), b(b), c(c) {}
+};
+
+
+int main() {
+    ST obj(19,-145.3, 'g');
+    serialization("temp.txt",&obj);
+
+    ST another_obj;
+    deserialization("temp.txt",&another_obj);
+
+    std::cout << another_obj.a << std::endl;
+    std::cout << another_obj.b << std::endl;
+    std::cout << another_obj.c << std::endl;
 }
--- Utils/SerDes/SerDes.h
+++ Utils/SerDes/SerDes.h
@@ -1,1 +1,37 @@
 #pragma once
+#include <fcntl.h>
+#include <string>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <cstring>
+
+template<class T>
+void serialization(const std::string& file, const T* obj ) {
+    int fd = open(file.c_str(), O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+
+    auto bytes = reinterpret_cast<const char*>(obj);
+    write(fd, bytes, sizeof(T));
+
+    close(fd);
+}
+
+template<class T>
+void deserialization(const std::string& file, T* obj) {
+    int fd = open(file.c_str(), O_RDONLY);
+    struct stat st{};
+    fstat(fd, &st);
+
+    auto t = mmap(0, st.st_size, PROT_READ, MAP_SHARED,
+        fd, 0);
+
+    if (t == MAP_FAILED) {
+        perror("mmap");
+        return;
+    }
+
+    std::memcpy(obj, t, st.st_size);
+
+    munmap(t, st.st_size);
+    close(fd);
+}
