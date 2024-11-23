364c5fabaca668c07deb35ed6c95dad303ab23dd
Gevorg <gevnavoyan@gmail.com>
2024-11-23 15:58:17 +0400

    Fix RAII

--- Demo/SerDes/SerDesDemo.cpp
+++ Demo/SerDes/SerDesDemo.cpp
@@ -6,8 +6,12 @@ struct ST {
     long long b;
     char c;
 
-    ST(){}
-    ST(unsigned int a, long long b, char c): a(a), b(b), c(c) {}
+    ST() : a(0), b(0), c('\0') {}
+    ST(unsigned int a, long long b, char c) : a(a), b(b), c(c) {}
+};
+
+struct Mec {
+    char m_data[1024*1024];
 };
 
 int main() {
@@ -48,5 +52,18 @@ int main() {
         std::cout<<"First field: " << obj.a <<" == "<< another_obj.a << std::endl;
         std::cout<<"Second field: "<< obj.b <<" == "<< another_obj.b << std::endl;
         std::cout<<"Third field: " << obj.c <<" == "<< another_obj.c << std::endl;
+    }
+    std::cout << "TEST 4\n";
+    {
+        Mec obj{};
+        for (size_t i = 0;i<1024*1024;i++) {
+            obj.m_data[i] = 'G';
+        }
+        serialization("temp4.txt",obj);
+
+        Mec another_obj{};
+        deserialization("temp4.txt",another_obj);
+
+        std::cout<<"First field: " << obj.m_data[1025] <<" == "<< another_obj.m_data[1025] << std::endl;
     }
 }
--- Utils/SerDes/SerDes.h
+++ Utils/SerDes/SerDes.h
@@ -5,51 +5,67 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <cstring>
+#include <filesystem>
+
+#define MAGIC_NUMBER ('R' + 'O' + 'B')
+class SmartMap {
+    unsigned char* data;
+    size_t length;
+    public:
+    SmartMap(void *addr, size_t length, int prot, int flags,
+                  int fd, off_t offset):length(length) {
+        auto t = mmap(addr, length, prot, flags, fd, offset);
+        if (t == MAP_FAILED) {
+            close(fd);
+            throw std::runtime_error("Failed to mmap file");
+        }
+        data = static_cast<unsigned char*>(t);
+    }
+    ~SmartMap() {
+        munmap(data, length);
+    }
+    unsigned char* get() const {
+        return data;
+    }
+};
 
 template<class T>
-void serialization(const std::string& file, const T& obj ) {
+void serialization(const std::filesystem::path& file, const T& obj ) {
+    static_assert(std::is_trivially_copyable_v<T>);
+
     int fd = open(file.c_str(), O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     if (fd == -1) {
         throw std::runtime_error("Failed to open file");
     }
 
-    size_t length = sizeof(T);
-    if (ftruncate(fd, length) == -1) {
+    constexpr size_t length = sizeof(T);
+    if (ftruncate(fd, length+1) == -1) {
         close(fd);
         throw std::runtime_error("Failed to resize file");
     }
 
-    void* t = mmap(nullptr, length,  PROT_WRITE, MAP_SHARED, fd, 0);
-    if (t == MAP_FAILED) {
-        close(fd);
-        throw std::runtime_error("Failed to mmap file");
-    }
-
-    std::memcpy(t, &obj, length);
-
-    munmap(t, length);
+    SmartMap t(nullptr, length+1,  PROT_WRITE, MAP_SHARED, fd, 0);
+    *t.get() = MAGIC_NUMBER;
+    std::memcpy(t.get()+1, &obj, length);
     close(fd);
 }
 
 template<class T>
-void deserialization(const std::string& file, T& obj) {
+void deserialization(const std::filesystem::path& file, T& obj) {
+    static_assert(std::is_trivially_copyable_v<T>);
+
     int fd = open(file.c_str(), O_RDONLY);
     if (fd == -1) {
         throw std::runtime_error("Failed to open file");
     }
+    constexpr size_t length = sizeof(T);
 
-    size_t length = sizeof(T);
+    SmartMap t(nullptr,length+1, PROT_READ, MAP_PRIVATE,fd, 0);
 
-    auto t = mmap(nullptr,length, PROT_READ, MAP_PRIVATE,
-                  fd, 0);
-
-    if (t == MAP_FAILED) {
-        close(fd);
-        throw std::runtime_error("Failed to mmap file");
+    if(*t.get() != MAGIC_NUMBER) {
+        throw std::runtime_error("Failed to deserialize file");
     }
+    std::memcpy(&obj, t.get()+1, length);
 
-    std::memcpy(&obj, t, length);
-
-    munmap(t, length);
     close(fd);
 }
