20e227baffdebc0814a76673380092a3035146d8
Gevorg <gevnavoyan@gmail.com>
2024-12-07 09:07:46 +0400

    Fix

--- Utils/DirectoryWatcher/DirectoryWatcher.h
+++ Utils/DirectoryWatcher/DirectoryWatcher.h
@@ -2,45 +2,51 @@
 #include <sys/inotify.h>
 #include <Directory/Directory.h>
 
-#define MAX_EVENTS 128
+#define MAX_EVENTS 8
 #define LEN_NAME 64
 #define EVENT_SIZE ( sizeof (struct inotify_event) )
 #define BUF_LEN ( MAX_EVENTS * ( EVENT_SIZE + LEN_NAME ))
 
 class DirectoryWatcher {
-    int fd;
+    int m_fd;
 
-    void parseChilds(const Directory& dir) {
-        unsigned mask = IN_ALL_EVENTS ;
+    void parseChildren(const Directory& dir) {
+        unsigned mask;
 
-        std::cout<< dir.get_path()<<std::endl;
-        auto t = inotify_add_watch(fd, dir.get_path().c_str(), mask);
+        if(dir.get_type() == Directory::Folder) {
+            mask = IN_CREATE | IN_DELETE;
+        }
+        else {
+            mask = IN_MODIFY;
+        }
+        auto t = inotify_add_watch(m_fd, dir.get_path().c_str(), mask);
         if (t == -1) {
             throw std::runtime_error( "cant add file/forlder");
         }
 
-        for (int i = 0; i< dir.size(); i++) {
-            parseChilds(dir[i]);
+        auto size = dir.size();
+        for (int i = 0; i < size; i++) {
+            parseChildren(dir[i]);
         }
     }
 
 public:
     explicit DirectoryWatcher(const std::string& path) {
-        fd = inotify_init();
-        if (fd < 0) {
+        m_fd = inotify_init();
+        if (m_fd < 0) {
             throw std::runtime_error( "Cant create inotify object");
         }
 
         Directory root(path);
-        parseChilds(root);
+        parseChildren(root);
 
     }
     ~DirectoryWatcher() {
-        close(fd);
+        close(m_fd);
     }
 
     void watch() {
-        char buffer[4096]
+        char buffer[BUF_LEN]
      __attribute__ ((aligned(__alignof__(struct inotify_event))));
         const struct inotify_event *event;
         unsigned len;
@@ -49,7 +55,7 @@ public:
         std::cout<<"<----START---->"<<std::endl;
         while(true)
         {
-            len = read(fd, buffer, sizeof buffer);
+            len = read(m_fd, buffer, sizeof buffer);
             if (len == -1 ) {
                 throw std::runtime_error( "Cant read from inotify object");
             }
@@ -63,7 +69,7 @@ public:
 
 
                 if ((event->mask & IN_CREATE) != 0) {
-                    std::cout<<"Created: "<< event->name<< std::endl;
+                    std::cout<<"Created: "<< event->name << std::endl;
                 }
                 if ((event->mask & IN_DELETE) != 0) {
                     std::cout<<"Deleted: "<< event->name << std::endl;
@@ -71,10 +77,6 @@ public:
                 if ((event->mask & IN_MODIFY) != 0) {
                     std::cout<<"Modify: "<< event->name <<  std::endl;
                 }
-                if ((event->mask & IN_MOVE) != 0) {
-                    std::cout<<"Move: "<< event->name <<  std::endl;
-                }
-
             }
         }
     }
