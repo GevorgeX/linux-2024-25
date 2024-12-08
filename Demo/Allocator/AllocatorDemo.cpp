7daae5f49844c93284ce6c980843fa7de21db893
Gevorg <gevnavoyan@gmail.com>
2024-12-08 17:11:04 +0400

    Implement Allocator

--- Demo/Allocator/AllocatorDemo.cpp
+++ Demo/Allocator/AllocatorDemo.cpp
@@ -1,5 +1,19 @@
+#include <vector>
 #include "Allocator/Allocator.h"
 
 int main() {
+    char buff[1000];
+    Allocator<int> allocator(buff, 1000);
+    std::vector<int, Allocator<int>> v(allocator);
 
+    for (int i = 0; i < 40; i++) {
+        v.push_back(i);
+    }
+
+    v.resize(20);
+    v.shrink_to_fit();
+    allocator.print();
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        std::cout << *it << " ";
+    }
 }
