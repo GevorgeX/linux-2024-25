49a12d62f10d5d3a1ee204a526f7b3b54998ec37
Gevorg Navoyan <gevnavoyan@gmail.com>
2025-02-12 19:06:10 +0400

    fix 2

--- Utils/Thread/BlockingQueue/BlockingQueue.h
+++ Utils/Thread/BlockingQueue/BlockingQueue.h
@@ -8,7 +8,8 @@ class BlockingQueue {
     std::queue<T> m_queue;
     std::mutex m_mtx;
     std::condition_variable m_cond_var;
-    size_t m_max_size;
+    const size_t m_max_size;
+    bool m_done = false;
 public:
     explicit BlockingQueue(size_t size):m_max_size(size) {
     }
@@ -18,20 +19,20 @@ public:
         m_cond_var.notify_all();
     }
 
-    void push(const T& t) noexcept {
+    void push(const T& t)  {
         std::unique_lock<std::mutex> lock{m_mtx};
         m_cond_var.wait(lock,
-                      [this] () { return m_queue.size() < m_max_size; }
+                      [this] () { return m_done || m_queue.size() < m_max_size; }
                       );
         m_queue.push(t);
         lock.unlock();
         m_cond_var.notify_one();
     }
 
-    void push(T&& t) noexcept {
+    void push(T&& t)  {
         std::unique_lock<std::mutex> lock{m_mtx};
         m_cond_var.wait(lock,
-                      [this] () { return m_queue.size() < m_max_size; }
+                      [this] () { return m_done || m_queue.size() < m_max_size; }
                       );
         m_queue.push(t);
         lock.unlock();
@@ -51,21 +52,26 @@ public:
 
     void try_push(const T& t)
     {
+        std::unique_lock<std::mutex> lock{m_mtx};
         m_queue.push(t);
     }
 
     void try_push(T&& t)
     {
+        std::unique_lock<std::mutex> lock{m_mtx};
         m_queue.push(t);
     }
 
     T try_pop()
     {
+        std::unique_lock<std::mutex> lock{m_mtx};
         auto value = m_queue.front();
+        m_queue.pop();
         return value;
     }
 
-    size_t size() const {
+    size_t size() {
+        std::unique_lock<std::mutex> lock{m_mtx};
         return m_queue.size();
     }
 
